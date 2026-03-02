108ee88a3bdc4d1545736dd505da964dae69ba5d
aposcus <ritiknegimail@gmail.com>
2026-03-02 14:49:56 +0530

    UC3 is done

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,17 @@
 public class PalindromeCheckerApp {
-    public static void main(String[] args)
-    {
-        System.out.println("welcome to palindrome checker app management system");
+    public static void main(String[] args) {
+
+        String original = "madam";
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        if (original.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
     }
 }
